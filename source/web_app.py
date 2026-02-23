f906a87231a991d3797215ae437d78d204f391d3
LoganMazurek <lmazurek42@gmail.com>
2026-02-22 23:14:04 -0600

    add buy me a coffee link and take flask out of debug mode

--- source/web_app.py
+++ source/web_app.py
@@ -1968,4 +1968,4 @@ if __name__ == '__main__':
     print(f"Starting server on http://127.0.0.1:5000")
     print("="*80)
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='127.0.0.1', port=5000)
