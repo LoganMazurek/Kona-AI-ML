322aa256b6d2243efe89e83987f62aaed544d4d2
LoganMazurek <lmazurek42@gmail.com>
2026-02-23 00:19:47 -0600

    Fix router template path

--- source/router_app.py
+++ source/router_app.py
@@ -4,8 +4,11 @@ Serves as the landing page for loganmazurek.com
 """
 
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Get the path to the templates directory (one level up from source/)
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
